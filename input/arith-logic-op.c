0cca22d4fb1703a8caa44df3384d8399ccad6514
lucasbastida <48137388+lucasbastida@users.noreply.github.com>
2021-11-30 20:34:18 -0300

    Updated input/ files

--- input/arith-logic-op.c
+++ input/arith-logic-op.c
@@ -1,20 +1,10 @@
-int q = 2;
+int q;
 int w;
 q=3;
 int x, y, z;
 int pepe2, pepa;
 int nros_primos;
 
-int x;
-double x = y;
-char a, b, c;
-int x = 5, y, z = x, w;
-
-
-x + ( 7 - 2) * fin2;
-5 || 6 && 5;
-while ( 4 && 6) {
-    7>45&&5<23 || (3 && 23 != d);
-
-    (6!= 5) * 3 || 1;
+main(){
+    return 0;
 }
--- input/ctl-struc.c
+++ input/ctl-struc.c
@@ -1,16 +1,53 @@
 int x;
-double x = y;
+double x = 2;
 char a, b, c;
-int x = 5, y, z = x, w;
-while (x < y) {
-    z = z + 5;
-    w = z;
+
+void add();
+
+void add()
+{
+    int d = 1;
+    d = d + 1;
 }
-for (x = 0; x < 10; x = x + 1)
-w = w + x;
-if (z == 0)
-    w = x;
-else {
-    w = z;
-    z = x;
+
+int main()
+{
+    int x = 5, y, z = x, w;
+    while (x < y)
+    {
+        z = z + 5;
+        w = z;
+    }
+    for (x = 0; x < 10; x = x + 1)
+        w = w + x;
+    if (z == 0)
+        w = x;
+    else
+    {
+        w = z;
+        z = x;
+    }
+    return 0;
 }
+
+void addconparam(int a, int b)
+{
+    int x = 5, y, z = x, w;
+    while (x < y)
+    {
+        z = z + 5;
+        w = z;
+    }
+    for (x = 0; x < 10; x = x + 1)
+        w = w + x;
+    if (z == 0)
+        w = x;
+    else
+    {
+        w = z;
+        z = x;
+    }
+    return;
+}
+
+void addconparam(int, int);
